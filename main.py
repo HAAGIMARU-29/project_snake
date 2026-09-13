4722d1c442542f3ccf635595af247e242a3d5aed
Av0hM <f20240494@goa.bits-pilani.ac.in>
2026-09-13 08:38:33 +0530

    Set Battlesnake bot metadata

--- main.py
+++ main.py
@@ -522,10 +522,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "SEDS Hackathon",
+        "color": "#2563EB",
+        "head": "sunglasses",
+        "tail": "bolt",
     }
 
 
